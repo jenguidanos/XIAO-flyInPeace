af6511d6c2f86ede2bb65eb6246bd1c5fc81dc2c
vabarca <viabfer@gmail.com>
2023-11-15 18:22:27 +0100

    Fix sound powerdown/powerup functionality

--- src/sound/src/ad9833/ad9833.cpp
+++ src/sound/src/ad9833/ad9833.cpp
@@ -20,22 +20,25 @@ err_code_t AD9833::setup()
     ad.begin();
     ad.setMode(MD_AD9833::MODE_SINE);
     pinMode(AD9833::_pin_shutdown,OUTPUT);
+    powerdown();
     return ERR_CODE_NONE;
 }
 
 void AD9833::set_vario(float vario)
 {
+    abs(vario) < 0.5f? powerdown(): powerup();
+    
     ad.setFrequency(MD_AD9833::CHAN_0, 200);
 }
 
 void AD9833::powerdown(void)
 {
-    digitalWrite(AD9833::_pin_shutdown, HIGH);
+    digitalWrite(AD9833::_pin_shutdown, LOW);
 }
 
 void AD9833::powerup(void)
 {
-    digitalWrite(AD9833::_pin_shutdown, LOW);
+    digitalWrite(AD9833::_pin_shutdown, HIGH);
 }
     
 //------------------------------------------------------------------------------
