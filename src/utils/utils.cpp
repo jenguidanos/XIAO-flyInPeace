c2303873084a3d5f4349b4b9e3f11c2bdde990ee
vabarca <viabfer@gmail.com>
2023-11-15 20:36:47 +0100

    Add moving average calculator class

---
+++ src/utils/utils.cpp
@@ -0,0 +1,46 @@
+/** 
+ *  @brief Utilities definitions
+ *  @author Cooked by Vicente A. (TT)
+ */
+
+//---[ Includes: ]--------------------------------------------------------------
+
+#include <cmath>
+#include "utils.h"
+
+//------------------------------------------------------------------------------
+
+err_code_t CMovAvg::setup()
+{
+    if(!_size) return ERR_CODE_SETUP;
+    _buffer = (float*)calloc(_size, sizeof(float));
+    return _buffer? ERR_CODE_NONE : ERR_CODE_SETUP;
+}
+
+void CMovAvg::update()
+{
+    _movAvg = CMovAvg::_calculate(_buffer, _size);
+}
+
+void CMovAvg::push(float val)
+{
+    if(!_buffer) return;
+    _buffer[_movAvgIndex] = val;
+    _movAvgIndex = (_movAvgIndex + 1) % _size;
+}
+
+float CMovAvg::_calculate(float *buff, int size)
+{
+    if(!buff) return 0.0f;
+    float sum = 0.0;
+    for(int i=0; i<size; i++) sum += buff[i];
+    return sum / size;
+}
+
+CMovAvg::~CMovAvg()
+{
+    if(_buffer)free(_buffer);
+    _buffer = nullptr;
+}
+
+//------------------------------------------------------------------------------
